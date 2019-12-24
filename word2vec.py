718ee4806d80d1b47d255834c880a6b886cffc9a
schmolle <jean-luc.schmolzi@web.de>
2019-12-24 14:52:30 +0100

    read from file

--- word2vec.py
+++ word2vec.py
@@ -1,1 +1,17 @@
-print("Hello")
+from gensim.models import Word2Vec
+
+def main():
+    file = open("/home/jschmolzi/txtFiles/1987.txt","r")
+    for i in range(3):
+        print(file.readLine())
+    
+def train_model(name):
+    print("Starting : " + name)
+    path = get_tmpfile("/home/jschmolzi/pModels/"+name+".model")
+    model = Word2Vec("/home/jschmolzi/txtFiles/"+name+".txt", size=100, window=5, min_count=5, workers=4)
+    model.save("/home/jschmolzi/pModels/1987.model")
+    print(name + " finished")
+    return(model)
+    
+if __name__ == '__main__':
+    main()
