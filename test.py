4f8ef890f4075bec2892c8f69d22750f7e864caf
schmolle <jean-luc.schmolzi@web.de>
2020-01-04 13:27:26 +0100

    added new model to test

--- test.py
+++ test.py
@@ -3,9 +3,12 @@ from gensim.test.utils import get_tmpfile
 
 def main():
     model1987 = Word2Vec.load("/home/jschmolzi/pModels/1987.model")
+    model2007 = Word2Vec.load("/home/jschmolzi/pModels/2007.model")
     result = model1987.most_similar(positive=['woman', 'king'], negative=['man'])
     print(result)
     result = model1987.most_similar(positive=['apple'],topn=5)
+    print(result)
+    result = model2007.most_similar(positive=['apple'],topn=5)
     print(result)
     
 if __name__ == '__main__':
