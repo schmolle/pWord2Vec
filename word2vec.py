2871e7e79d79a60a0bd820e87652ec256f5421f7
schmolle <jean-luc.schmolzi@web.de>
2020-01-04 10:55:13 +0100

    added saving

--- word2vec.py
+++ word2vec.py
@@ -1,6 +1,7 @@
 from gensim.models import Word2Vec
 
 def main():
+    path = get_tmpfile("/home/jschmolzi/pModels/1987.model")
     model = Word2Vec(size=100, window=5, min_count=1, workers=4)
     with open("/home/jschmolzi/txtFiles/1987.txt","r") as file:
         line = file.readline()
@@ -8,10 +9,10 @@ def main():
             sentences = line.strip().split('.')
             print(sentences)
             for sentence in sentences:
-                # model.train(sentence.split(),epochs=1)
-                print(sentence.split())
+                model.train(sentence.split(),epochs=1)
             line = file.readline()
-
+    model.save("/home/jschmolzi/pModels/1987.model")
+    
 def train_model(name):
     print("Starting : " + name)
     path = get_tmpfile("/home/jschmolzi/pModels/"+name+".model")
