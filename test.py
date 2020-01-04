8c210201c12c5729bc23642434b89050ec7ee60e
schmolle <jean-luc.schmolzi@web.de>
2020-01-04 11:54:51 +0100

    .

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def main():
     model = Word2Vec.load("/home/jschmolzi/pModels/1987.model")
     result = model.most_similar(positive=['woman', 'king'], negative=['man'])
     print(result)
-    result = word_vectors.most_similar(positive=['apple'],topn=5)
+    result = model.most_similar(positive=['apple'],topn=5)
     print(result)
     
 if __name__ == '__main__':
