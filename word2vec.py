3f87ba76ec14432b68de0a6600ababc03bae3619
schmolle <jean-luc.schmolzi@web.de>
2020-01-04 11:22:12 +0100

    .

--- word2vec.py
+++ word2vec.py
@@ -3,16 +3,16 @@ from gensim.test.utils import get_tmpfile
 
 def main():
     path = get_tmpfile("/home/jschmolzi/pModels/1987.model")
-    model = Word2Vec(size=100, window=5, min_count=1, workers=4)
     with open("/home/jschmolzi/txtFiles/1987.txt","r") as file:
         line = file.readline()
+        all_sentences=[]
         while line:
             sentences = line.strip().split('.')
             print(sentences)
             for sentence in sentences:
-                model.build_vocab(sentence.split(),update=True)
-                model.train(sentence.split(),total_examples=1,epochs=1)
+                all_sentences.append(sentence.split())
             line = file.readline()
+    model = Word2Vec(all_sentences,size=100, window=5, min_count=1, workers=4)
     model.save("/home/jschmolzi/pModels/1987.model")
     
 def train_model(name):
