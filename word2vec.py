a4df34740a34fd8910258b72f4cfef65e1e77392
schmolle <jean-luc.schmolzi@web.de>
2020-01-04 10:48:24 +0100

    changing loop

--- word2vec.py
+++ word2vec.py
@@ -3,13 +3,15 @@ from gensim.models import Word2Vec
 def main():
     model = Word2Vec(size=100, window=5, min_count=1, workers=4)
     with open("/home/jschmolzi/txtFiles/1987.txt","r") as file:
-        for line in enumerate(file):
+        line = fp.readline()
+        while line:
             sentences = line.strip().split('.')
             print(sentences)
             for sentence in sentences:
                 # model.train(sentence.split(),epochs=1)
                 print(sentence.split())
-    
+            line = fp.readline()
+
 def train_model(name):
     print("Starting : " + name)
     path = get_tmpfile("/home/jschmolzi/pModels/"+name+".model")
