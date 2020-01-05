cbe44356053a04c8c246f232b5f4674ca31aed66
schmolle <jean-luc.schmolzi@web.de>
2020-01-05 10:28:18 +0100

    .

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from gensim.test.utils import get_tmpfile
 def main():
     model1987 = Word2Vec.load("/home/jschmolzi/pModels/1987.model")
     model2007 = Word2Vec.load("/home/jschmolzi/pModels/2007.model")
-    while(true):
+    while(True):
         word = input("compare word : ")
         result = model1987.most_similar(positive=[word],topn=20)
         print(result)
