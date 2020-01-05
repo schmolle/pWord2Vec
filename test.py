a5b807e3ab151beedf6b136d8ea1fe9bc169e5eb
schmolle <jean-luc.schmolzi@web.de>
2020-01-05 10:26:59 +0100

    changed tests

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ from gensim.test.utils import get_tmpfile
 def main():
     model1987 = Word2Vec.load("/home/jschmolzi/pModels/1987.model")
     model2007 = Word2Vec.load("/home/jschmolzi/pModels/2007.model")
-    result = model1987.most_similar(positive=['woman', 'king'], negative=['man'])
-    print(result)
-    result = model1987.most_similar(positive=['apple'],topn=5)
-    print(result)
-    result = model2007.most_similar(positive=['apple'],topn=5)
-    print(result)
+    while(true):
+        word = input("compare word : ")
+        result = model1987.most_similar(positive=[word],topn=20)
+        print(result)
+        result = model2007.most_similar(positive=[word],topn=20)
+        print(result)
     
 if __name__ == '__main__':
     main()
