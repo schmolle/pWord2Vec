eefdc154ecf5384b09243a9271a6796d911447e5
schmolle <jean-luc.schmolzi@web.de>
2020-01-06 17:53:59 +0100

    .

--- word2vec.py
+++ word2vec.py
@@ -6,15 +6,25 @@ def main():
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     path = get_tmpfile("/home/jschmolzi/pModels/all.model")
     all_sentences=[]
-    print("prepare data")
-    with open("/home/jschmolzi/txtFiles/all.txt","r") as file:
-        print("start reading file")
-        for line in file:
-            sentences = line.strip().split('.')
-            for sentence in sentences:
-                all_sentences.append(sentence.split())
-    print("data ready")
-    model = Word2Vec(all_sentences,size=100, window=5, min_count=1, workers=4)
+    model = Word2Vec(size=100, window=5, min_count=5, workers=4)
+    print("VOCAB BUILDING")
+    for i in range(1987,2008):
+        with open("/home/jschmolzi/txtFiles/"+i+".txt","r") as file:
+            print("start reading file : "+i)
+            for line in file:
+                sentences = line.strip().split('.')
+                for sentence in sentences:
+                    all_sentences.append(sentence.split())
+        model.build_vocab(all_sentences,update=True)
+    print("TRAINING")
+    for i in range(1987,2008):
+        with open("/home/jschmolzi/txtFiles/"+i+".txt","r") as file:
+            print("start reading file : "+i)
+            for line in file:
+                sentences = line.strip().split('.')
+                for sentence in sentences:
+                    all_sentences.append(sentence.split())
+        model.train(all_sentences)
     model.save("/home/jschmolzi/pModels/all.model")
     
 if __name__ == '__main__':
