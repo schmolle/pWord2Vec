7754d58451cd990135666f4bd469ef21f66363e3
schmolle <jean-luc.schmolzi@web.de>
2020-01-06 11:10:10 +0100

    .

--- word2vec.py
+++ word2vec.py
@@ -6,7 +6,7 @@ def main():
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     path = get_tmpfile("/home/jschmolzi/pModels/1987.model")
     all_sentences=[]
-    with open("/home/jschmolzi/txtFiles/all.txt","r") as file:
+    with open("/home/jschmolzi/txtFiles/1987.txt","r") as file:
         line = file.readline()
         while line:
             sentences = line.strip().split('.')
@@ -14,7 +14,7 @@ def main():
                 all_sentences.append(sentence.split())
             line = file.readline()
     model = Word2Vec(all_sentences,size=100, window=5, min_count=1, workers=4)
-    model.save("/home/jschmolzi/pModels/all.model")
+    model.save("/home/jschmolzi/pModels/1987.model")
     
 if __name__ == '__main__':
     main()
