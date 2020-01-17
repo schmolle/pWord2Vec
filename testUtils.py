9f66ca4c4ca933899ca3e3253efe77469b01ab48
schmolle <jean-luc.schmolzi@web.de>
2020-01-17 11:55:03 +0100

    .

--- testUtils.py
+++ testUtils.py
@@ -1,12 +1,12 @@
 import sentenceUtils
 
 def main():
-    line = 'Hello; this- is. me? or! not'
-    print("split_line(" + line + ") -> ")
-    print(sentenceUtils.split_line(line))
-    line = "John's boOk iS losT"
-    print("clean_up_line(" + line + ") -> ")
-    print(sentenceUtils.clean_up_line(line))
-    
+    line = "HeL,lo; tHi's- is. m\"e? or! not"
+    print("Line : " + line)
+    line = sentenceUtils.clean_up_line(line)
+    print("Line after cleanup : " + line) 
+    line = sentenceUtils.split_line(line)
+    print("Line after split : ")
+    print(line)    
 if __name__ == '__main__':
     main()
