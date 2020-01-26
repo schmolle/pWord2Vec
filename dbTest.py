015d93a6b4e8a06dd31a20480d460dd040bdd0ab
schmolle <jean-luc.schmolzi@web.de>
2020-01-26 06:57:33 +0100

    .

--- dbTest.py
+++ dbTest.py
@@ -5,7 +5,7 @@ def main():
     cursor = connection.cursor()
     query = "select * from words"
     print(db.fetchFromDb(cursor,query))
-    print(db.getIdFromWord(cursor,"apple"))
+    print(db.getIdFromWord(cursor,'apple'))
     print(db.getWordFromId(cursor,1))
     cursor.close()
     connection.close()
