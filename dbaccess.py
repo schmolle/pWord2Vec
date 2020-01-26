4b40db4d86ee42c363b23705b90beceab3b7fb8b
schmolle <jean-luc.schmolzi@web.de>
2020-01-26 06:42:05 +0100

    .

--- dbaccess.py
+++ dbaccess.py
@@ -17,8 +17,8 @@ def commitToDb(connection,cursor,query):
     connection.commit()
 
 def getIdFromWord(cursor,word):
-    query = sql.SQL("select id from words where word = (word)",word)
-    return fetchFromDb(cursor, query)
+    cursor.execute("select id from words where word = (word)",word)
+    return cursor.fetchall()
 
 def getWordFromId(cursor,id):
     query = sql.SQL("select word from words where id = (id)",id)
