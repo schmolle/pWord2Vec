59e165b3366379a27154cecb76d189f320b96590
schmolle <jean-luc.schmolzi@web.de>
2020-01-26 06:29:46 +0100

    .

--- dbTest.py
+++ dbTest.py
@@ -3,6 +3,8 @@ import dbaccess as db
 def main():
     connection = db.getConnection()
     cursor = connection.cursor()
+    query = "select * from words"
+    print(fetchFromDb(cursor,query))
     print(db.getIdFromWord(cursor,"apple"))
     print(db,getWordFromId(cursor,1))
     
