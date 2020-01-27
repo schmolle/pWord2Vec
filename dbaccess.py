4bd9cad9760b9a9d2829eb6700905d3fca2816f7
schmolle <jean-luc.schmolzi@web.de>
2020-01-27 11:03:33 +0100

    .

--- dbaccess.py
+++ dbaccess.py
@@ -37,6 +37,7 @@ def insertVector(connection,cursor,setting,word,year,vector):
     wordId = insertWord(connection,cursor,word)
     for idx,value in enumerate(vector):
         insertVectorValue(connection,cursor,wordId,settingsId,year,idx,value)
+    connection.commit()
 
 def insertVectorValue(connection,cursor,wordId,settingsId,year,dimension,value):
     cursor.execute("INSERT INTO vectors(settingsId,wordId,year,dimension,value) VALUES(%s, %s, %s, %s, %s)",
