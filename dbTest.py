801d9de808bb782e983b74736471342e8f7277a1
schmolle <jean-luc.schmolzi@web.de>
2020-01-28 13:45:45 +0100

    testing vector access

--- dbTest.py
+++ dbTest.py
@@ -4,8 +4,7 @@ def main():
     connection = db.getConnection()
     cursor = connection.cursor()
     vector=[1.5,2.5,3.3,4.2,21.1]
-    db.insertVector(connection,cursor,'setting2','word2',2007,vector)
-    db.insertVector(connection,cursor,'setting1','word1',2000,vector)
+
     cursor.close()
     connection.close()
     
--- test.py
+++ test.py
@@ -4,14 +4,13 @@ from gensim.test.utils import get_tmpfile
 def main():
     model1987 = Word2Vec.load("/home/jschmolzi/pModels/1987.model")
     model2007 = Word2Vec.load("/home/jschmolzi/pModels/2007.model")
-    while(True):
-        word = input("compare word : ")
-        result1987 = model1987.most_similar(positive=[word],topn=20)
-        result2007 = model2007.most_similar(positive=[word],topn=20)
-        i=0
-        for r1,r2 in zip(result1987,result2007):
-            print("{0:2d} :  {1}  {2:.5f} |  {3}  {4:.5f}".format(i,r1[0].ljust(15),r1[1],r2[0].ljust(15),r2[1]))
-            i = i+1
-            
+    vecs1987 = Word2Vec.wv.vectors
+    i=0
+    for vec in vecs1987:
+        print(vec)
+        i = i+1
+        if i >10 :
+            break;
+                
 if __name__ == '__main__':
     main()
