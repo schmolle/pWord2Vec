fce01e601d4e2d5f70941ab853dde00729629be9
schmolle <jean-luc.schmolzi@web.de>
2020-01-28 14:45:47 +0100

    .

--- dbaccess.py
+++ dbaccess.py
@@ -20,7 +20,7 @@ def insertWord(connection,cursor,word):
     cursor.execute("INSERT INTO words(word) VALUES (%s)\
                     ON CONFLICT DO NOTHING",(word,))
     connection.commit()
-    return "Inserted word %s",(word,)
+    return "Inserted word %s" %(word)
 
 def getWordId(cursor,word):
     cursor.execute("SELECT wordId from words WHERE word = %s",(word,))
@@ -30,7 +30,7 @@ def insertSetting(connection,cursor,setting):
     cursor.execute("INSERT INTO settings(setting) VALUES (%s)\
                     ON CONFLICT DO NOTHING",(setting,))
     connection.commit()
-    return "Inserted Setting %s",(setting,)
+    return "Inserted Setting %s" %(setting)
 
 def getSettingId(cursor,setting):
     cursor.execute("SELECT settingsId from settings WHERE setting = %s",(setting,))
@@ -44,7 +44,8 @@ def insertVector(connection,cursor,setting,word,year,vector):
     connection.commit()
 
 def insertVectorValue(connection,cursor,wordId,settingsId,year,dimension,value):
-    cursor.execute("INSERT INTO vectors(settingsId,wordId,year,dimension,value) VALUES(%s, %s, %s, %s, %s)",
+    cursor.execute("INSERT INTO vectors(settingsId,wordId,year,dimension,value) VALUES(%s, %s, %s, %s, %s) \
+                    ON CONFLICT DO NOTHING",
                    (settingsId, wordId, year, dimension, value, ))
 
 def getIdFromWord(cursor,word):
