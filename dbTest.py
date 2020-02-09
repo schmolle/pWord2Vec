c5a34efb480e5d1fecbc7e96aff4e46c326b0ce7
schmolle <jean-luc.schmolzi@web.de>
2020-02-09 09:52:56 +0100

    .

--- dbTest.py
+++ dbTest.py
@@ -15,8 +15,17 @@ def main():
     #print(model.similarity('players', 'mary'))
     #print(numpy.dot(vec1, vec2) / (numpy.linalg.norm(vec1) * numpy.linalg.norm(vec2)))
     #print(numpy.dot(oVec1, oVec2) / (numpy.linalg.norm(oVec1) * numpy.linalg.norm(oVec2)))
+    bestId
+    bestCosSim = 2
     wordIds = db.getWordIdsFromYear(cursor,6,1987)
-    print(wordIds[:20])
+    for wordId in words:
+        vec1 = db.getVecotr(cursor,6,1987,wordId)
+        vec2 = db.getVecotr(cursor,6,1988,wordId)
+        cosSim = numpy.dot(vec1, vec2) / (numpy.linalg.norm(vec1) * numpy.linalg.norm(vec2))
+        if cosSim < bestCosSim :
+            bestId = wordId
+            bestCosSim = cosSim
+    print(bestId , bestCosSim)
     cursor.close()
     connection.close()
     
