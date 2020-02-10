44d61618c9674b1a72162e910cef6b24401af0df
schmolle <jean-luc.schmolzi@web.de>
2020-02-10 09:24:30 +0100

    added timestamps

--- dbTest.py
+++ dbTest.py
@@ -3,8 +3,11 @@ from gensim.models import Word2Vec
 from gensim.test.utils import get_tmpfile
 import numpy
 import numpy.linalg
+import time
+from _sqlite3 import connect
 
 def main():
+    start = time.time()
     connection = db.getConnection()
     cursor = connection.cursor()
     #vec1 = db.getVector(cursor,6,1987,2224)
@@ -15,9 +18,11 @@ def main():
     #print(model.similarity('players', 'mary'))
     #print(numpy.dot(vec1, vec2) / (numpy.linalg.norm(vec1) * numpy.linalg.norm(vec2)))
     #print(numpy.dot(oVec1, oVec2) / (numpy.linalg.norm(oVec1) * numpy.linalg.norm(oVec2)))
+    connection = time.time()
     bestId = 0
     bestCosSim = 2
     wordIds = db.getWordIdsFromYear(cursor,6,1987)
+    word = time.time()
     for wordId in wordIds:
         vec1 = db.getVector(cursor,6,1987,wordId)
         vec2 = db.getVector(cursor,6,1988,wordId)
@@ -26,8 +31,14 @@ def main():
             bestId = wordId
             bestCosSim = cosSim
     print(bestId , bestCosSim)
+    eval=time.time()
     cursor.close()
     connection.close()
-    
+    end = time.time()
+    print("connection : " , connect-start)
+    print("words: : " , words-connect)
+    print("eval : " , eval - words)
+    print("full : " , end -start)
+
 if __name__ == '__main__':
     main()
